2d3d42b4901743cb56e9dec20a9043adac3eb30f
rukia <ruqayashar1@gmail.com>
2023-04-05 15:25:41 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie" ,"Aki"];
+
+function writeCards(){
+    const response = []
+    let i;
+    for(i= 0; i <gifts.length; i++){
+response[i] = `Thank you, ${gifts[i]}, for the wonderful surprise gift!`
+    }
+    return response;
+}
+
+function countDown(){
+    let i = 10;
+    while (i>=0)
+    console.log(i--);
+}
+countDown()
